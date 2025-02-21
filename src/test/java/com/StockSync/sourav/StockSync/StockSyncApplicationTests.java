a2641c710bb9466910e457d4c686f2a26fe06ddf
Sourav-Kumar-Nayak <souravkumarnayak041@gmail.com>
2025-02-22 00:09:57 +0530

    First push

---
+++ src/test/java/com/StockSync/sourav/StockSync/StockSyncApplicationTests.java
@@ -0,0 +1,13 @@
+package com.StockSync.sourav.StockSync;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockSyncApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
